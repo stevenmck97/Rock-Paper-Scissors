d7e6036991b58de657e0525d23d0f261608e6626
Steven McKeown <stevenmckeown19@gmail.com>
2022-02-10 18:34:16 +0000

    Added eventlisteners to each button. Creaded handleClick function to handle which button was pressed. Passed a string into each handleClick call with either rock paper or scissors to be used in playRound function

--- rps.js
+++ rps.js
@@ -12,9 +12,26 @@ function computerPlay() {
     return randomChoice;
 }
 
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+btnRock.addEventListener('click', () => playRound(handleClick('rock'), computerSelection));
+btnPaper.addEventListener('click', () => playRound(handleClick('paper'), computerSelection));
+btnScissors.addEventListener('click', () => playRound(handleClick('scissors'), computerSelection));
+
+let handleClick = (playerSelection) => {
+    if (playerSelection === 'rock') {
+        return 'rock'
+    } else if (playerSelection === 'paper') {
+        return 'paper'
+    } else if (playerSelection === 'scissors') {
+        return 'scissors'
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Rock, Paper, or Scissors?: ')
-    playerSelection = playerSelection.toLowerCase();
+    
+    
     if (playerSelection == computerSelection) {
         console.log('No winners! Play again.')
         roundNumber++;
@@ -29,8 +46,8 @@ function playRound(playerSelection, computerSelection) {
         
     } else if 
     ((computerSelection == 'rock' && playerSelection == 'scissors') || 
-    (computerSelection == 'paper' && playerSelection =='rock') ||
-    (computerSelection == 'scissors' && playerSelection == 'paper') 
+    (computerSelection == 'paper' && playerSelection == 'rock') ||
+    (computerSelection == 'scissors' && playerSelection == 'scissors') 
     ) {
         roundNumber++;
         computerScore = computerScore+1;
@@ -45,3 +62,29 @@ function playRound(playerSelection, computerSelection) {
 // }
 
 //         game();
+
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = prompt('Rock, Paper, or Scissors?: ')
+//     playerSelection = playerSelection.toLowerCase();
+//     if (playerSelection == computerSelection) {
+//         console.log('No winners! Play again.')
+//         roundNumber++;
+//     } else if 
+//     ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+//     (playerSelection == 'paper' && computerSelection =='rock') ||
+//     (playerSelection == 'scissors' && computerSelection == 'paper')
+//     ) { 
+//         roundNumber++;
+//         playerScore = playerScore+1;
+//         console.log('You win! Your score is: ' + playerScore)
+        
+//     } else if 
+//     ((computerSelection == 'rock' && playerSelection == 'scissors') || 
+//     (computerSelection == 'paper' && playerSelection =='rock') ||
+//     (computerSelection == 'scissors' && playerSelection == 'paper') 
+//     ) {
+//         roundNumber++;
+//         computerScore = computerScore+1;
+//         console.log('You lose! The computer\'s score is: ' + computerScore)
+//     } 
+// }
